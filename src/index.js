21da19c2dd479cf105e56d1e05f647dcc2ef7909
Bendik Solheim <bendik@solheim.im>
2018-02-04 21:18:23 +0100

    Load a route from data repository and display on map

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import mapboxgl from 'mapbox-gl';
 import './app.css';
 
+const dataBaseUrl = 'https://bendiksolheim.github.io/landevei.bike-data';
+
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYmVuZGlrc29saGVpbSIsImEiOiJjamQzbHl6YXowdnZjMnFvcXJoMXNwcjRqIn0.Azuz2KugpSMCBwKSe0yc9Q';
 const map = new mapboxgl.Map({
@@ -8,4 +10,26 @@ const map = new mapboxgl.Map({
   container: 'app',
   style: 'mapbox://styles/bendiksolheim/cjd526ulr414d2ssc72yp9fku',
   zoom: 13
+});
+
+const byneset = {
+  type: 'geojson',
+  data: `${dataBaseUrl}/byneset.json`
+};
+
+map.on('load', () => {
+  map.addSource('byneset', byneset);
+  map.addLayer({
+    id: 'route',
+    type: 'line',
+    source: 'byneset',
+    layout: {
+      'line-join': 'round',
+      'line-cap': 'round'
+    },
+    paint: {
+      'line-color': '#000',
+      'line-width': 4
+    }
+  });
 });
